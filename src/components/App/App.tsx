9112b24a95087c180cb1a205b56252bb1aaaac01
ramiro elisei <ramiroelisei@gmail.com>
2023-11-07 19:45:06 +0100

    Add Route HomePage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "../../pages/HomePage/HomePage";
 
 const App = (): React.ReactElement => {
   return (
     <main className="main-container">
       <Routes>
         <Route path="/" element={<Navigate to="/movies" />} />
-        <Route path="/movies" />
+        <Route path="/movies" element={<HomePage />} />
       </Routes>
     </main>
   );
