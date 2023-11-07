d4034216a57bf5c89887bf4c4e20969b6771c391
ramiro elisei <ramiroelisei@gmail.com>
2023-11-07 16:40:27 +0100

    Add useMoviesApi custom hook

---
+++ src/hooks/useMoviesApi.ts
@@ -0,0 +1,17 @@
+import { useCallback } from "react";
+import { MovieStructure } from "../store/features/movies/types";
+
+const useMovieApi = () => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const getMovies = useCallback(async () => {
+    const response = await fetch(`${apiUrl}?_limit=5`);
+    const movies = (await response.json()) as MovieStructure;
+
+    return movies;
+  }, [apiUrl]);
+
+  return { getMovies };
+};
+
+export default useMovieApi;
