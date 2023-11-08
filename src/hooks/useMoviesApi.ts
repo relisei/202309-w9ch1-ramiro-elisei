fd0a1b929520df83104ced2546eb4232a344e2ba
ramiro elisei <ramiroelisei@gmail.com>
2023-11-08 10:58:41 +0100

    Fix path in fetch useMoviesApi

--- src/hooks/useMoviesApi.ts
+++ src/hooks/useMoviesApi.ts
@@ -5,7 +5,7 @@ const useMovieApi = () => {
   const apiUrl = import.meta.env.VITE_API_URL;
 
   const getMovies = useCallback(async () => {
-    const response = await fetch(`${apiUrl}movies?_limit=5`);
+    const response = await fetch(`${apiUrl}/movies?_limit=5`);
     const movies = (await response.json()) as MovieStructure[];
 
     return movies;
